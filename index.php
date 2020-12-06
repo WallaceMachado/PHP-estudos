8ecac471e1bd55a3d3d50ff357faac39d853f39f
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-06 20:45:08 -0300

    Olá Mundo - html php

--- index.php
+++ index.php
@@ -20,9 +20,13 @@
                 <div class="modulo verde">
                     <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="exercicio.php">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">
+                                Olá PHP
+
+                            </a>
+                        </li>
+                       
                     </ul>
                 </div>
                 <div class="modulo vermelho">
