2285779cc2c915dacb09a3948296cfc1018c9c5c
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-06 17:12:41 -0300

    Navgear entres telas inicial e exercicio, conteudo da tela execicio - html css

--- exercicio.php
+++ exercicio.php
@@ -15,12 +15,17 @@
         <h2>Visualização do Exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href="#" class="verde">Sem formatação</a>
+        <a href=<?= "/{$_GET['dir']}/{$_GET['file']}.php" ?> 
+        class="verde">Sem formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
-
+        <?php
+             include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php"); 
+             /*http://localhost:8080/exercicio.php?dir=teste&file=teste*/
+            // include('teste/teste.php')
+            ?>
         </div>
     </main>
     <footer class="rodape"> 
