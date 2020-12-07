cec722d9414ad5c2db5dbf3b1e7e1be1868fd9a7
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-07 14:05:47 -0300

    Tipos de dados

---
+++ tipos/string.php
@@ -0,0 +1,38 @@
+<div class="titulo">Tipo String</div>
+
+<?php
+echo 'Eu sou uma string';
+echo '<br>';
+var_dump("Eu também");
+echo '<br>';
+
+// concatenação
+echo("Nós também" . ' somos' . '<br>');
+echo("Número é " . 123);
+echo '<br>', "Também aceito", " virgulas";
+
+echo '<br>';
+echo "'Teste' " . '"Teste" ' . '\'Teste\' ' . "\"Teste\" " . "\\";
+
+print("<br>Também existe o função print");
+print "<br>Também existe o função print";
+
+// Algumas funções
+echo '<br>' . strtoupper('maximizado');
+echo '<br>' . strtolower('MINIMIZADO');
+echo '<br>' . ucfirst('só a primeira letra');
+echo '<br>' . ucwords('todas as palavras');
+echo '<br>' . strlen('Quantas letras?');
+echo '<br>' . mb_strlen("Eu também", "utf-8");
+echo '<br>' . substr('Só uma parte mesmo', 7, 6); // parte
+echo '<br>' . str_replace('isso', 'aquilo', 'Trocar isso isso');
+
+// Enunciado:
+// Avaliando os métodos da documentação da string, 
+// qual o métodos que a posicao do texto 'abc' 
+// na string '!AbcaBcabc' retorne 1?
+
+echo strpos('!AbcaBcabc', 'abc') . '<br>';
+echo stripos('!AbcaBcabc', 'abc') . '<br>';
+
+echo strpos(strtolower('!AbcaBcabc'), strtolower('ABC'));   
