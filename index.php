0532143878d7839c3089b20def43e0ab755d820a
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-13 15:45:46 -0300

    function-metodo - MAP e FILTER

--- index.php
+++ index.php
@@ -289,6 +289,11 @@
                                 Retornando Função
                             </a>
                         </li>
+                        <li>
+                            <a href="exercicio.php?dir=funcoes&file=map_filter">
+                                Map & Filter
+                            </a>
+                        </li>
                     </ul>
                 </div>
                 <div class="modulo azul-escuro">
