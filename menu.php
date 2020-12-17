841148d14c5611182d9fea9152adb787bc70f2d2
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-17 11:53:13 -0300

    API - datas

--- menu.php
+++ menu.php
@@ -435,4 +435,19 @@
             </li>
             </ul>
                 </div>
+                <div class="modulo azul">
+        <h3>13. API</h3>
+        <ul>
+            <li>
+                <a href="exercicio.php?dir=api&file=datas_01">
+                    Datas #01
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=api&file=datas_02">
+                    Datas #02
+                </a>
+            </li>
+            </ul>
+                </div>
             </nav>
