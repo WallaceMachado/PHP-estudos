af0d2bfbaf1c9b12355bfac22f46a96b84a49082
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-17 16:57:48 -0300

    arquivos - carregar na tela as imagens salvas

---
+++ api/imagens.php
@@ -0,0 +1,44 @@
+<div class="titulo">Imagens</div>
+
+<?php 
+error_reporting(E_ERROR);  // mostrar somente erros e não advertencias
+session_start();
+
+$arquivos = $_SESSION['arquivos'] ?? [];// inicia com um array vazio 
+
+$pastaUpload = __DIR__ . '/../files/';// pasta interna 
+$nomeArquivo = $_FILES['arquivo']['name'];
+$arquivo = $pastaUpload . $nomeArquivo;// concatena o diretorio com o nome do arquivo
+$tmp = $_FILES['arquivo']['tmp_name'];
+if (move_uploaded_file($tmp, $arquivo)) {// pega o arquivo temporario e armazena no arquivo defoult
+    echo "<br>Arquivo válido e enviado com sucesso.";
+    $arquivos[] = $nomeArquivo;
+    $_SESSION['arquivos'] = $arquivos;
+} else {
+    echo "<br>Erro no upload de arquivo!";
+}
+// abaixo para suportar upload de arquivos em html
+?>
+
+<form action="#" method="post"
+    enctype="multipart/form-data">
+    <input name="arquivo" type="file">
+    <button>Enviar</button>
+</form>
+
+<ul>
+    <?php foreach($arquivos as $arquivo): // pecorrer o array com os arquivos?>
+        <?php if(stripos($arquivo, '.png') > 0): // filtrar somente os arquivos .png  e abaixo carrega o arquivo .png na tela?>
+            <li>
+                <img src="../files/<?= $arquivo ?>"
+                    height="120" />
+            </li>
+        <?php endif ?>
+    <?php endforeach ?>
+</ul>
+
+<style>
+    input, button {
+        font-size: 1.2rem;
+    }
+</style>
