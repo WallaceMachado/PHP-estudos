65b20ce6e9bd172549839f84b2f02513b8b0c65e
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-18 17:21:55 -0300

    banco de dados - alterar cadastro

--- menu.php
+++ menu.php
@@ -525,6 +525,11 @@
                     Inserir Registro #02
                 </a>
             </li>
+            <li>
+                <a href="exercicio.php?dir=db&file=alterar">
+                    Alterar Registro
+                </a>
+            </li>
             </ul>
        
                 </div>
