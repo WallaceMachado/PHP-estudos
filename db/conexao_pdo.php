4dc80a677a4bb2162d6dbef67d19549d2dc37967
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-19 09:12:04 -0300

    banco de dados - conexao pdo

---
+++ db/conexao_pdo.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ERROR); // mostrar somente erros e não advertencias
+function novaConexao($banco = 'curso_php') {
+    $servidor = '127.0.0.1:3307';
+    $usuario = 'root';
+    //$senha = 'root';
+
+    try {
+        $conexao = new PDO("mysql:host=$servidor;dbname=$banco",
+            $usuario, $senha);
+        return $conexao;
+    } catch(PDOException $e) {
+        die('Erro: ' . $e->getMessage());
+    }
+}
+
+//novaConexao();
+//echo "Fim!";
